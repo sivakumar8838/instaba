5f2395ea3846efc28d612dc4b6d6fbae23d96fe4
siva <sivamss5882@gmail.com>
2024-03-31 19:48:11 +0530

    hi

--- controller/Phones.js
+++ controller/Phones.js
@@ -5,21 +5,14 @@ const phoneRouter = express.Router();
 const phonecontroller = {
     phone: async (req, res) => {
         try{
-            const phones = await Phone.find()
+            const {id} = req.params;
+            const phones = await Phone.findById(id)
             return res.json(phones);
         }catch(error){
             console.error(error);
             return res.json({message:error.message});
         }
     },
-    // getphone: async (req, res) => {
-    //     try{
-    //    const {id} = req.params;
-    //    const songs = await 
-    //     }catch(error){
-
-    //     }
-    // }
 }
-phoneRouter.get('/', phonecontroller.phone);
+phoneRouter.get('/:id', phonecontroller.phone);
 module.exports = phoneRouter;
